336022dc2e4a0707349d993283d8eff3e291c842
victer-star <keita1273.zzz@gmail.com>
2022-02-13 07:39:27 +0900

    inputTitle調整前にもどす

--- src/components/menu/MenuCardTitle.jsx
+++ src/components/menu/MenuCardTitle.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 export const MenuCardTitle = () => {
   const [isClick, setIsClick] = useState(false);
-  const [inputCardTitle, setInputCardTitle] = useState("");
+  const [inputCardTitle, setInputCardTitle] = useState("メニュー名");
 
   const handleClick = () => {
     setIsClick(true);
@@ -30,7 +30,6 @@ export const MenuCardTitle = () => {
         <form onSubmit={handleSubmit}>
           <input
             className="menuCardTitleInput"
-            placeholder="メニュー名入力欄"
             autoFocus
             type="text"
             onChange={handleChange}
